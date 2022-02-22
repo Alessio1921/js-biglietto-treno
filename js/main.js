a0a51082abefb9648ed611aa749b1646c0b0a988
Alessio Caruso <caruso.alessio1999@gmail.com>
2022-02-22 16:22:43 +0100

    exercise completed

--- js/main.js
+++ js/main.js
@@ -1,22 +1,27 @@
-let numberOfKm = prompt("Quanti kilometri vuole percorrere?");
-let eta = prompt ("qual'è la vostra eta?")
-
+const numberOfKm = parseInt (prompt("Quanti kilometri vuole percorrere?"));
+const eta = parseInt( prompt("qual'è la vostra eta?"));
 let priceTicket = numberOfKm * 0.21;
 let discount;
 
-if (eta < 18)
+document.getElementById("km").innerHTML= numberOfKm + "km";
+document.getElementById("eta").innerHTML=eta;
+
+if ( isNaN(numberOfKm) || isNaN (eta))
 {
-  discount = ((priceTicket/100)*20);
-  priceTicket -= discount;
-  console.log(priceTicket.toFixed(2) + "€");
-}
-else if(eta > 65)
-{
-  discount = ((priceTicket/100)*40);
-  priceTicket -= discount;
-  console.log(priceTicket.toFixed(2) + "€");
+  console.warn("devi inserire solo i numeri");
+  document.getElementById("price").innerHTML= "Hai inserito dei caratteri non validi, ricarica la pagina";
 }
 else
 {
-  console.log(priceTicket + "€");
+  if (eta < 18)
+  {
+    discount = ((priceTicket/100)*20);
+    priceTicket-= discount ;
+  }
+  else if (eta >= 65)
+  {
+    discount = ((priceTicket/100)*40);
+    priceTicket -= discount;
+  }
+  document.getElementById("price").innerHTML=priceTicket.toFixed(2) + "€";
 }
